e547ff678149e8ab60123f587152db62b501daee
장영훈 <jyh08024@gmail.com>
2024-08-19 02:21:11 +0900

    Publishing branch

---
+++ android/app/src/main/kotlin/com/example/drive_mate_real/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.drive_mate_real
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/page/splash.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+
+  const SplashScreen({ super.key });
+
+  @override
+  State<StatefulWidget> createState() => SplashState();
+}
+
+class SplashState extends State<SplashScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
