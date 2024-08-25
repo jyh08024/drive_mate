e4d4536721810663b8c4f3e0798eb64fd12b568e
장영훈 <80209790+jyh08024@users.noreply.github.com>
2024-08-26 08:03:59 +0900

    Update home.dart

--- lib/page/main/home.dart
+++ lib/page/main/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../utils/pallete.dart';
 
@@ -13,11 +17,48 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
 
+  Future<void> getCarList() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    var url = Uri.http('10.0.2.2', '/carList');
+    var response = await http.get(url);
+
+    await prefs.setString('carList', response.body);
+  }
+
+  Future<void> getPref() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    print(prefs.getString('carList'));
+  }
 
   @override
   Widget build(BuildContext context) {
+    final String svgPath = 'assets/images/car_inner.svg';
+    final Widget svg = SvgPicture.asset(
+        svgPath,
+        colorFilter: ColorFilter.mode(Colors.red, BlendMode.xor),
+    );
+
     return Scaffold(
       backgroundColor: Palette.whiteMain.color,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 100),
+            ElevatedButton(
+                onPressed: () {
+                  getCarList();
+                }, child: Text('shared 저장')
+            ),
+            const SizedBox(height: 100),
+            ElevatedButton(
+                onPressed: () {
+                  getPref();
+                }, child: Text('shared 블러우기')
+            ),
+            svg,
+          ],
+        ),
+      )
     );
   }
 }
